2bbfc5f9d2fb06d12171caaa43bc01577081839f
Habook2456 <tonyto.m.2456@gmail.com>
2025-06-17 00:52:25 -0500

    fix duplicate routes in StudentRoutes.php

--- routes/custom/StudentsRoutes.php
+++ routes/custom/StudentsRoutes.php
@@ -11,7 +11,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('students/registrar', [StudentController::class, 'create'])->name('students.enroll');
     Route::post('students/registrar', [StudentController::class, 'store'])->name('students.enroll.store');
-    Route::delete('students/registrar/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+    // Route::delete('students/registrar/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
 
     Route::get('/students/{id}/carnet', [StudentController::class, 'generateCarnetPdf'])->name('students.carnet');
     Route::get('/students/selected/carnets', [StudentController::class, 'generateSelectedCarnetsPdf'])->name('students.selected.carnets');
@@ -19,8 +19,8 @@ Route::middleware(['auth'])->group(function () {
     // routes/web.php
     Route::get('/students/export', [StudentController::class, 'export'])->name('students.export');
     Route::post('/students/import', [StudentController::class, 'import'])->name('students.import');
-    Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
-    Route::post('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+    // Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    // Route::post('/students/{id}', [StudentController::class, 'update'])->name('students.update');
 
 
     Route::get('/students/{id}/attendance-report-pdf', [StudentController::class, 'attendanceReportPdf'])->name('students.attendance-report-pdf');
